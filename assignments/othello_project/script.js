8eac041781febea95d27c9be5d4f92bc0fa811e8
Andy Kim <andyjinkim@gmail.com>
2015-10-12 11:20:09 -0700

    pieces able to be placed, working on flipping pieces on click

--- assignments/othello_project/script.js
+++ assignments/othello_project/script.js
@@ -1,22 +1,32 @@
 counter = 0;
 var $boxes = $('.box');
+var boxes = document.querySelectorAll('.box');
+var $columns = $('.column1')
 
-function placePiece(el) {
-  console.log('testing');
+function placePiece() {
   $boxes.on('click', function(){
       console.log('counter', counter);
       if ((this).className === "box") {
         if (counter % 2 === 0) {
-          (this).className = "black";
-          (this).innerHTML = ""
+          (this).className = "box black";
         } else {
-          (this).className = "white";
-          (this).innerHTML = ""
+          (this).className = "box white";
         }
-        console.log($boxes);
-        counter = counter + 1;
+        // counter = counter + 1;
+        // console.log(counter);
       }
     })
   }
 
 placePiece();
+
+//hint: jquery hasClass()
+
+
+
+// for (i=0; i<boxes.length; i++) {
+//   console.log('for loop function firing');
+//   if ((this).className === "box black" && boxes[i+4].className === "box white" && boxes[i+8].className === "box black") {
+//     boxes[i+4].toggleClass("box black");
+// }
+// }
