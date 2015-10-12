f9bcf562e34f40d3539a8a9590cb2b5986cd72c4
Andy Kim <andyjinkim@gmail.com>
2015-10-12 16:44:54 -0700

    black toggle works, white doesnt

--- assignments/othello_project/script.js
+++ assignments/othello_project/script.js
@@ -9,14 +9,54 @@ function placePiece() {
       if ((this).className === "box") {
         if (counter % 2 === 0) {
           (this).className = "box black";
+
+          var row = parseInt($(this).data('row'));
+          var column = parseInt($(this).data('col'));
+          var $entireRow = $('.box[data-row="' + row + '"]')
+
+          function checkBoxes() {
+            $entireRow.each(function() {
+              if ($(this).not(document.getElementsByClassName('black'))) {
+                $(this).removeClass('white').addClass('black');
+              }
+              console.log($(this).hasClass('white'));
+            })
+          }
+
         } else {
           (this).className = "box white";
+
+          // function checkWhite() {
+          //   $entireRow.each(function() {
+          //     if ($(this).not(document.getElementsByClassName('white'))) {
+          //       $(this).removeClass('black').addClass('white')
+          //     }
+          //   })
+          // }
+
         }
-        // counter = counter + 1;
+        counter = counter + 1;
         // console.log(counter);
       }
-    })
-  }
+
+
+
+    // function checkBoxes() {
+    //   if (($('.box[data-row="' + row '"]')).hasClass("white") {
+    //     toggleClass("box black")
+    //   })
+    // }
+
+    console.log('Row: ', row);
+    console.log('Column: ', column);
+    console.log('All divs in same row: ', $('.box[data-row="' + row + '"]'))
+    //console.log('All divs in same row: ', $('.box.row'+row));
+    console.log('All divs in same column: ', $( ".box[data-col='" + column + "']" ))
+    // console.log($('.box[data-row="' + (row - 1) + '"][data-col="' + (column + 1) + '"]'))
+
+    checkBoxes();
+  })
+}
 
 placePiece();
 
