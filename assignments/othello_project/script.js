a1105d4cb089c2fe9d7453ae972a1317dc7969cc
Andy Kim <andyjinkim@gmail.com>
2015-10-12 18:31:22 -0700

    black and white toggle working vertical and horizontal

--- assignments/othello_project/script.js
+++ assignments/othello_project/script.js
@@ -3,28 +3,43 @@ var $boxes = $('.box');
 var boxes = document.querySelectorAll('.box');
 var $columns = $('.column1')
 
-function placePiece() {
-  $boxes.on('click', function(){
-      console.log('counter', counter);
-      if ((this).className === "box") {
-        if (counter % 2 === 0) {
-          (this).className = "box black";
 
-          var row = parseInt($(this).data('row'));
-          var column = parseInt($(this).data('col'));
-          var $entireRow = $('.box[data-row="' + row + '"]')
+$boxes.on('click', function(){
+    counter = counter + 1;
+    console.log('counter', counter); {
 
-          function checkBoxes() {
+        var row = parseInt($(this).data('row'));
+        var column = parseInt($(this).data('col'));
+        var $entireRow = $('.box[data-row="' + row + '"]')
+        var $entireColumn = $( ".box[data-col='" + column + "']" )
+
+        function checkBoxes() {
+          if (counter % 2 === 0) {
+            (this).className = "box black";
             $entireRow.each(function() {
               if ($(this).not(document.getElementsByClassName('black'))) {
                 $(this).removeClass('white').addClass('black');
+            };
+          });
+            $entireColumn.each(function() {
+              if ($(this).not(document.getElementsByClassName('black'))) {
+                $(this).removeClass('white').addClass('black');
               }
-              console.log($(this).hasClass('white'));
             })
-          }
-
-        } else {
+        }
+        else if (counter % 2 === 1) {
           (this).className = "box white";
+            $entireRow.each(function() {
+              if ($(this).not(document.getElementsByClassName('white'))) {
+                $(this).removeClass('black').addClass('white');
+              };
+            });
+            $entireColumn.each(function() {
+              if ($(this).not(document.getElementsByClassName('white'))) {
+                $(this).removeClass('black').addClass('white');
+              }
+            })
+      }
 
           // function checkWhite() {
           //   $entireRow.each(function() {
@@ -33,10 +48,8 @@ function placePiece() {
           //     }
           //   })
           // }
-
-        }
-        counter = counter + 1;
-        // console.log(counter);
+      }
+      checkBoxes();
       }
 
 
@@ -54,11 +67,10 @@ function placePiece() {
     console.log('All divs in same column: ', $( ".box[data-col='" + column + "']" ))
     // console.log($('.box[data-row="' + (row - 1) + '"][data-col="' + (column + 1) + '"]'))
 
-    checkBoxes();
+    // checkBoxes();
   })
-}
 
-placePiece();
+
 
 //hint: jquery hasClass()
 
